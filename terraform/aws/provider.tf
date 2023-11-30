f84eefc8bc6e0d5e25d1c56229e118215f52d561
Marcelo Henrique Borgas <marcelo.borgas@edu.fecap.br>
2023-11-29 21:05:47 -0300

    a

---
+++ terraform/aws/provider.tf
@@ -0,0 +1,29 @@
+# PROVIDER
+terraform {
+
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.34"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "bucketdecria"
+    key            = "terraform.tfstate"
+    dynamodb_table = "dynamodecria"
+    
+    region         = "us-east-1"
+  }
+
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
